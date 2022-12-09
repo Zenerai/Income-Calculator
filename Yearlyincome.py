04e1f28d103d999429e5d4dbce77a96c05169417
Dustin Hill <117607995+Zenerai@users.noreply.github.com>
2022-12-08 21:53:19 -0700

    Uploading files

---
+++ Yearlyincome.py
@@ -0,0 +1,21 @@
+income = input('How much does the job pay hourly or yearly? $')
+
+# yearly and hourly evaluate what math should be completed.
+yearly = 4 <= len(income)
+hourly = 2 >= len(income)
+
+if yearly:
+    per_month = round(int(income) / 12, 2)
+    per_week = int(per_month) / 4
+    per_hour = int(per_week) / 40
+    print(f'Pay per month:${per_month} \nPay per week: ${per_week} \nPay per hour: ${per_hour} (Assuming 40 hours)')
+
+elif hourly:
+    hours = input('How many hours per week? ')
+    per_hour = int(income)
+    per_week = int(per_hour) * int(hours)
+    per_month = int(per_week) * 4
+    per_year = int(per_month) * 12
+    print(f"Pay per hour: $", per_hour , "\nPay per week: $", per_week, f"(At {hours} hours)", "\nPay per month: $",
+          per_month, "\nPay per year $", per_year)
+
